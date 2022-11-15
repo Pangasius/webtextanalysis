5cccba4ddeca75cbbfd0cc264c4dfbff8f0abf95
Pangasius <lizepirenne@gmail.com>
2022-11-15 15:27:33 +0100

    testing from a new machine, fixing compatibility

--- glove_run.py
+++ glove_run.py
@@ -7,17 +7,14 @@ if(not os.path.exists(sys.path[0] + "/../GloVe")):
     raise Exception("GloVe folder not found : path to download : https://github.com/stanfordnlp/GloVe")
 
 for file in ["source", "target"]:
-    #make sure required file demo_custom.sh is in the glove folder
-    if(not os.path.exists(sys.path[0] + "/../GloVe/demo_custom.sh")):
-        print("Path to GloVe custom runnable not found" + sys.path[0] + "/../GloVe/demo_custom.sh")
-        subprocess.run("cp " + sys.path[0] + "/demo_custom.sh " + sys.path[0] + "/../GloVe/demo_custom.sh", shell=True)
-
     first_move = "cp -f " + sys.path[0] + '/samples/' + file + '.txt' + " " + sys.path[0] + '/../GloVe/' + "tokenised.txt";
     move_sh = "cp -f " + sys.path[0] + '/demo_custom.sh' + " " + sys.path[0] + '/../GloVe/' + "demo_custom.sh";
     call_glove = "cd " + sys.path[0] + '/../GloVe/' + " && ./demo_custom.sh";
     second_move = "cp -f " + sys.path[0] + '/../GloVe/' + "vectors.txt " + sys.path[0] + '/models/' + file + '_glove.txt';
 
     join_commands = first_move + " && " + call_glove + " && " + second_move;
+    
+    print("Running command : " + join_commands)
 
     process  = subprocess.Popen(join_commands, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     out, err = process.communicate()
@@ -29,4 +26,4 @@ for file in ["source", "target"]:
         print(line)
         
     if len(out.splitlines()) == 0:
-        raise Exception("GloVe call failed, possibly because not run from a batch file")
+        raise Exception("GloVe call failed. Usually permissions on demo_custom.sh is an issue")
